6d564de1be259a45b92317f7ecca34eaa4bdcc92
AntonyGonzales <80794253+AntonyGonzales@users.noreply.github.com>
2021-04-27 21:54:55 -0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PracticaN2.Models;
+
+namespace PracticaN2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult FormularioContacto()
+        {
+            return View();
+        }
+
+        [HttpPost]
+         public IActionResult FormularioContacto(Contacto a){
+           
+            if(ModelState.IsValid){             
+
+                return RedirectToAction("Index");
+
+            }
+            return View(a);
+        }
+
+        public IActionResult Productos()
+        {
+            return View();
+        }
+
+        public IActionResult IniciarSesion()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
