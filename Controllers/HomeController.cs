861a3dc89bb57a6e5f7e0ab51ff6a088a514039d
Antony Gonzales <tommy201464@hotmail.com>
2021-04-27 19:42:52 -0500

    p02

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,13 +1,13 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using pokedex.Models;
+using PI_ATERIA.Models;
 
-namespace pokedex.Controllers
+namespace PI_ATERIA.Controllers
 {
     public class HomeController : Controller
     {
