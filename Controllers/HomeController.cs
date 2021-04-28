2cec45a0c927912bef2c06a72079cb27c2665e6f
Antony Gonzales <tommy201464@hotmail.com>
2021-04-27 22:03:32 -0500

    pc02

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -53,7 +53,7 @@ namespace PracticaN2.Controllers
         {
             return View();
         }
-
+        //
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
